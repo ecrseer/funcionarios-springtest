1a04cff5de2005e85e90b58716658975d76ae48b
Gabriel Justino Marques Couto <gabriel.justino@dsgtechnology.com.br>
2024-06-20 23:39:41 -0300

    tests

--- src/main/java/br/gj/infnetat/gabjustino_funcionario_at/funcionario/FuncionarioController.java
+++ src/main/java/br/gj/infnetat/gabjustino_funcionario_at/funcionario/FuncionarioController.java
@@ -1,5 +1,6 @@
 package br.gj.infnetat.gabjustino_funcionario_at.funcionario;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,17 +8,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping( "funcionarios")
+@RequestMapping("funcionarios")
 public class FuncionarioController {
 
-    private FuncionarioService funcionarioService;
+    private final FuncionarioService funcionarioService;
+
     public FuncionarioController(FuncionarioService funcionarioService) {
         this.funcionarioService = funcionarioService;
     }
 
     @PostMapping
-    public Funcionario save(@RequestBody Funcionario funcionario) {
-        return this.funcionarioService.salvarFuncionario(funcionario);
+    public ResponseEntity<Funcionario> save(@RequestBody Funcionario funcionario) {
+        Funcionario salvo = this.funcionarioService.salvarFuncionario(funcionario);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(salvo);
     }
 
 }
--- src/test/java/br/gj/infnetat/gabjustino_funcionario_at/TestaFuncionariosController.java
+++ src/test/java/br/gj/infnetat/gabjustino_funcionario_at/TestaFuncionariosController.java
@@ -1,37 +1,59 @@
 package br.gj.infnetat.gabjustino_funcionario_at;
 
+
+
 import br.gj.infnetat.gabjustino_funcionario_at.funcionario.Funcionario;
 import br.gj.infnetat.gabjustino_funcionario_at.funcionario.FuncionarioController;
 import br.gj.infnetat.gabjustino_funcionario_at.funcionario.FuncionarioService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@WebMvcTest(FuncionarioController.class)
 public class TestaFuncionariosController {
 
-    @Mock
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
     private FuncionarioService funcionarioService;
 
-    @InjectMocks
-    private FuncionarioController funcionarioController;
-
-    @BeforeEach
-    public void setUp() {
-        System.out.println("Iniciando TestaFuncionariosController");
-        MockitoAnnotations.openMocks(this);
-    }
+    @Autowired
+    private ObjectMapper mapper;
 
     @Test
-    void testaSalvar(){
+    void testaSalvar() throws Exception {
         Funcionario mockFuncionario = new Funcionario("Gabri");
-        when(funcionarioController.save(mockFuncionario)).thenReturn(new Funcionario());
-        Funcionario respostaController = funcionarioController.save(mockFuncionario);
-        assertEquals(respostaController.getNome(), mockFuncionario.getNome());
+
+        given(
+                funcionarioService.salvarFuncionario(any(Funcionario.class))
+        ).willReturn(mockFuncionario);
+
+        mockMvc.perform(
+                post("/funcionarios")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(mockFuncionario))
+        )
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+
+
 
     }
 }
